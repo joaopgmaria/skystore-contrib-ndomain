1899f56569ae56d7e58870bdf3d432b41116be9d
João Maria <joao.maria@sky.uk>
2017-09-28 23:33:25 +0100

    Convert NDomain.Sql and NDomain.Log4Net WIP

--- source/NetCore2/NDomain.Log4Net/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NDomain.Log4Net
-{
-    public class Class1
-    {
-    }
-}
--- source/NetCore2/NDomain.Sql/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NDomain.Sql
-{
-    public class Class1
-    {
-    }
-}
